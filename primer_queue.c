06b350c3bf6145a3d6a3f8709c39f8d6227024c9
JLob Lap <joselobatof@gmail.com>
2020-07-06 21:30:32 -0500

    Primer queue implementado en array de 5

---
+++ primer_queue.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define SIZE 5
+int items[SIZE], front = -1, rear = -1;
+
+void enQueue(int item){
+    if(rear == SIZE-1){
+        printf("Nuestro queue esta lleno. \n ");
+    }else{
+        if (front == -1){
+            front = 0;
+        }
+        rear++;
+        items[rear] = item;
+        printf("Se inserto el valor %d correctamente. \n ",item);
+    }
+}
+
+void deQueue(){
+    if(front == -1){
+        printf("Nuestro queue esta vacio. \n ");
+    }else{
+        printf("Se elimino el valor %d correctamente. \n ", items[front]);
+        
+        for(int i = 0; i <= rear; i++){
+            items[i]= items[i+1];
+        }
+        rear--;
+        if (rear == -1){
+            front = -1;
+        }
+
+    }
+}
+
+void printQueue(){
+    printf(" Inicio de Queue. \n");
+        for(int i = 0; i <= rear; i++){
+            printf("%d -> %d \n",i,items[i]);
+        }
+    printf(" Fin de Queue. \n");
+}
+
+main(){
+    enQueue(10);
+    enQueue(12);
+    enQueue(13);
+    enQueue(15);
+    enQueue(17);
+    printQueue();
+    enQueue(18);
+    printQueue();
+    deQueue();
+    printQueue();
+    enQueue(18);
+    printQueue();
+    deQueue();
+    deQueue();
+    deQueue();
+    deQueue();
+    deQueue();
+    printQueue();
+    deQueue();
+    printQueue();
+    enQueue(19);
+    enQueue(20);
+    return 0;
+}
